1871286a4209b16114dc5d9f65d5d2adcd2adbaa
usmailaabdoul <ismaelabdul77@gmail.com>
2020-08-24 00:14:43 +0100

    adding a project section and shadow to the nav bar

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -17,14 +17,14 @@ const Home = () => {
   return (
     <div>
       <Carousels />
-      <section className="bg-gray-100 flex justify-between align-center px-20 ">
+      <section className="bg-gray-100 flex justify-between items-center px-20 ">
         <div style={styles.bodyInfo}>
           <h3 className="text-teal-500">Join the BUEA islamic community</h3>
           <p className="text-lg">Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu.
             Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum</p>
         </div>
 
-        <div style={styles.contactInfo} className="bg-teal-400">
+        <div style={styles.contactInfo} className="bg-teal-400 py-10 rounded">
           <h5 className="text-teal-100 font-medium text-3xl">Contact us to join</h5>
           <p className="text-teal-100 font-medium text-xl my-3">+237 671411017/ +237 691212173</p>
           <a href="/contact" className="bg-red-500 px-4 py-3 my-3 text-xl rounded text-red-100 hover:text-red-100 hover:bg-red-400" style={styles.a}>contact us</a>
@@ -37,14 +37,14 @@ const Home = () => {
           <h6>date: 14/08/2020</h6>
           <div>
             <p className="text-lg">Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu.
-             Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum
-              moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu.
-              Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei.
+            Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum
+            moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu.
+            Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei.
            No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu.</p>
             <p className="text-lg">Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu.
             Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum
-             moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu.
-             Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei.
+            moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu.
+            Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei.
            No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu.</p>
           </div>
         </div>
@@ -93,15 +93,40 @@ const Home = () => {
             {
               prayerTimes.map((prayerTime, key) => {
                 return (
-                <div key={key} style={styles.prayerTimes}>
-              <p style={styles.prayerTimeTitle}>{prayerTime.type1}</p>
-                  <div>
-                    <p className="text-2xl">{prayerTime.type2}</p>
-                    <p className="bg-teal-400 mx-8 py-2 rounded ">{prayerTime.time}</p>
+                  <div key={key} style={styles.prayerTimes}>
+                    <p style={styles.prayerTimeTitle}>{prayerTime.type1}</p>
+                    <div>
+                      <p className="text-2xl">{prayerTime.type2}</p>
+                      <p className="bg-teal-400 mx-8 py-2 rounded ">{prayerTime.time}</p>
+                    </div>
                   </div>
-                </div>
-                ) })
+                )
+              })
             }
+          </div>
+        </div>
+      </section>
+
+      <section className="py-4">
+        <div className="p-10">
+          <h4 className="text-center mt-5 mb-3 text-gray-900 font-medium text-3xl">PROGRAMS AND PROJECTS</h4>
+          <p className="text-center text-xl mx-32">Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos</p>
+        </div>
+        <div className="mx-20 my-3">
+          <div className="w-1/4 bg-gray-100 shadow-md rounded">
+            <img src={img1} alt="headerimg" className="w-full h-64 rounded-t " />
+            <div className="flex p-2">
+              <p>icn</p>
+              <p className="px-2 font-medium">20 AUG 2019</p>
+            </div>
+            <div className="m-auto py-4">
+              <h5 className="text-center">NEW MOSQUE PROJECT</h5>
+              <p className="text-center">Lorem ipsum dolor sit amet et delectus accommodare his consul.read more</p>
+
+              <div className="flex justify-center items-center">
+                <a href="/programs" className="bg-teal-500 shadow-md rounded text-gray-100 text-lg font-medium py-2 px-3 hover:text-gray-300 hover:bg-red-500 transition ease-in duration-150" style={styles.a}>Details</a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -158,6 +183,7 @@ const styles = {
     borderRadius: '5px 5px 0px 0px',
   }
 }
+
 const Carousels = () => {
   const [carouselInfos] = useState([
     {
